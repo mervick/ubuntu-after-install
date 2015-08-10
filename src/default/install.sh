fc95fa84bcf216a0eaa259d159ba98546766492f
Andrey Izman <izman.dev@gmail.com>
2015-08-10 16:07:53 +0300

    Update install.sh

--- src/default/install.sh
+++ src/default/install.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
 
+echo ""
+echo "################################"
+echo "### Add repositories ###########"
+echo "################################"
+
+while read line; do
+    if test "${#line}" -gt 0; then
+        if test "${line:0:1}" != "#"; then
+            data=()
+            while IFS='=' read -ra ADDR; do
+                for i in "${ADDR[@]}"; do
+                    data+=("$(echo -e "${i}" | sed -e 's/^[[:space:]]*//' | sed -e 's/[[:space:]]*$//')")
+                done
+            done <<< "$line"
+
+            if test "${#data[@]}" -gt 1; then
+                if test "${data[0]}" == "PPA"; then
+                    sudo add-apt-repository ppa:"${data[1]}"
+
+                elif test "${data[0]}" == "REPO"; then
+                    sudo add-apt-repository "${data[1]}"
+
+                elif test "${data[0]}" == "KEY"; then
+                    wget -q -O - "${data[1]}" | sudo apt-key add -
+
+                elif test "${data[0]}" == "SOURCE"; then
+                    if test "${#data[@]}" -gt 2; then
+                        sudo sh -c "echo \"${data[1]}\" >> /etc/apt/sources.list.d/${data[2]}.list"
+                    fi
+                fi
+            fi
+        fi
+    fi
+done < repositories.txt
+
+
+echo ""
+echo "################################"
+echo "### Update & Upgrade ###########"
+echo "################################"
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+
+echo ""
+echo "################################"
+echo "### Install packages ###########"
+echo "################################"
 
 ## Repositories ##
 # multimedia & codecs
