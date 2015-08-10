2445bf649aba124a75f4d8e112cabc8f1ec277cc
Andrey Izman <izman.dev@gmail.com>
2015-08-10 16:27:12 +0300

    Update install.sh

--- src/default/install.sh
+++ src/default/install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+
+echo ""
+echo "################################"
+echo "### Delete unused packages #####"
+echo "################################"
+
+while read line; do
+    if test "${#line}" -gt 0; then
+        if test "${line:0:1}" != "#"; then
+            echo "sudo apt-get purge {$line}"
+            sudo apt-get purge "{$line}"
+        fi
+    fi
+done
+
+
 echo ""
 echo "################################"
 echo "### Add repositories ###########"
@@ -17,17 +33,21 @@ while read line; do
 
             if test "${#data[@]}" -gt 1; then
                 if test "${data[0]}" == "PPA"; then
+                    echo "sudo add-apt-repository ppa:${data[1]}"
                     sudo add-apt-repository ppa:"${data[1]}"
 
                 elif test "${data[0]}" == "REPO"; then
+                    echo "sudo add-apt-repository ${data[1]}"
                     sudo add-apt-repository "${data[1]}"
 
                 elif test "${data[0]}" == "KEY"; then
+                    echo "wget -q -O - ${data[1]} | sudo apt-key add -"
                     wget -q -O - "${data[1]}" | sudo apt-key add -
 
                 elif test "${data[0]}" == "SOURCE"; then
                     if test "${#data[@]}" -gt 2; then
-                        sudo sh -c "echo \"${data[1]}\" >> /etc/apt/sources.list.d/${data[2]}.list"
+                        echo "sudo sh -c \"echo \\\"${data[1]}\\\" >> \\\"/etc/apt/sources.list.d/${data[2]}.list\\\"\""
+                        sudo sh -c "echo \"${data[1]}\" >> \"/etc/apt/sources.list.d/${data[2]}.list\""
                     fi
                 fi
             fi
@@ -133,13 +153,7 @@ sudo apt-get install oracle-java8-installer -y
 sudo apt-get install google-chrome-stable -y
 
 
-## remove extra packages ##
-# Orca Screen Reader
-sudo apt-get purge gnome-orca
-# Zeitgeist Datahub
-sudo apt-get purge zeitgeist zeitgeist-core zeitgeist-datahub
-# Deja dup
-sudo apt-get purge deja-dup
+
 
 
 # clear
